7f8cee94fb294c52c0205f372e1c1d82b0414768
Diego Reis <diegoreso@me.com>
2025-06-05 18:47:19 -0300

    feat: project created

---
+++ src/main/java/dev/reso/recipeit/RecipeItApplication.java
@@ -0,0 +1,13 @@
+package dev.reso.recipeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/reso/recipeit/RecipeItApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reso.recipeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
