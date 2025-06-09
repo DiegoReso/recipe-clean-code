9bd51e9878764cf5bb460468e024df26ab3b2cd2
Diego Reis <diegoreso@me.com>
2025-06-09 19:56:14 -0300

    feat: adds FindRecipesUseCase for context

--- src/main/java/dev/reso/recipeit/infra/beans/BeanConfiguration.java
+++ src/main/java/dev/reso/recipeit/infra/beans/BeanConfiguration.java
@@ -1,8 +1,10 @@
 package dev.reso.recipeit.infra.beans;
 
 import dev.reso.recipeit.core.gateway.RecipeGateway;
-import dev.reso.recipeit.core.useCases.CreatesRecipeCase;
-import dev.reso.recipeit.core.useCases.CreatesRecipeImpl;
+import dev.reso.recipeit.core.useCases.CreatesRecipeUseCase;
+import dev.reso.recipeit.core.useCases.CreatesRecipeUseCaseImpl;
+import dev.reso.recipeit.core.useCases.FindRecipesUseCase;
+import dev.reso.recipeit.core.useCases.FindRecipesUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
 
     @Bean
-    public CreatesRecipeCase createsRecipe(RecipeGateway recipeGateway){
-        return new CreatesRecipeImpl(recipeGateway);
+    public CreatesRecipeUseCase createsRecipe(RecipeGateway recipeGateway){
+        return new CreatesRecipeUseCaseImpl(recipeGateway);
+    }
+
+    @Bean
+    public FindRecipesUseCase findRecipesUseCase(RecipeGateway recipeGateway){
+        return new FindRecipesUseCaseImpl(recipeGateway);
     }
 }
