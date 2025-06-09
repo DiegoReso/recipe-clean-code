eb9ba702f6382579a0a853c88ffc17a243e0ea5a
Diego Reis <diegoreso@me.com>
2025-06-09 20:00:12 -0300

    feat: creates the findRecipes method

--- src/main/java/dev/reso/recipeit/core/gateway/RecipeGateway.java
+++ src/main/java/dev/reso/recipeit/core/gateway/RecipeGateway.java
@@ -2,8 +2,12 @@ package dev.reso.recipeit.core.gateway;
 
 import dev.reso.recipeit.core.entities.Recipe;
 
+import java.util.List;
+
 public interface RecipeGateway {
 
     Recipe createsRecipe(Recipe recipe);
+
+    List<Recipe> findRecipes();
 
 }
