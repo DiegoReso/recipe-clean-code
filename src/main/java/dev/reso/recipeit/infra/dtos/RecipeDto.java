27ef501d23526c61ea27b56bac2663170bf90d0b
Diego Reis <diegoreso@me.com>
2025-06-09 18:53:09 -0300

    feat: creates RecipeDto

---
+++ src/main/java/dev/reso/recipeit/infra/dtos/RecipeDto.java
@@ -0,0 +1,17 @@
+package dev.reso.recipeit.infra.dtos;
+
+import dev.reso.recipeit.core.enums.TypeRecipe;
+
+import java.time.LocalDateTime;
+
+public record RecipeDto(
+        Long id,
+        String name,
+        String description,
+        String instruction,
+        double preparationTimeMinutes,
+        double servings,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt,
+        TypeRecipe type
+) {}
