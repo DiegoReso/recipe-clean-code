9454f62684bda9ed4a26e09420986fccf46c9236
Diego Reis <diegoreso@me.com>
2025-06-09 19:58:01 -0300

    feat: creates getmapping to find all recipes and injects findRecipesUseCase

--- src/main/java/dev/reso/recipeit/infra/controller/RecipeController.java
+++ src/main/java/dev/reso/recipeit/infra/controller/RecipeController.java
@@ -1,21 +1,22 @@
 package dev.reso.recipeit.infra.controller;
 
 import dev.reso.recipeit.core.entities.Recipe;
-import dev.reso.recipeit.core.useCases.CreatesRecipeCase;
+import dev.reso.recipeit.core.useCases.CreatesRecipeUseCase;
+import dev.reso.recipeit.core.useCases.FindRecipesUseCase;
 import dev.reso.recipeit.infra.dtos.RecipeDto;
 import dev.reso.recipeit.infra.mapper.RecipeDtoMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/recipe")
 @RequiredArgsConstructor
 public class RecipeController {
 
-    private final CreatesRecipeCase createsRecipeCase;
+    private final CreatesRecipeUseCase createsRecipeCase;
+    private final FindRecipesUseCase findRecipesUseCase;
     private final RecipeDtoMapper mapper;
 
 
@@ -23,6 +24,11 @@ public class RecipeController {
     public RecipeDto createRecipe(@RequestBody RecipeDto recipeDto){
         Recipe recipe = createsRecipeCase.execute(mapper.toRecipe(recipeDto));
         return mapper.toRecipeDto(recipe);
+    }
+
+    @GetMapping
+    public List<RecipeDto> findAllRecipes(){
+        return findRecipesUseCase.excute().stream().map(mapper::toRecipeDto).toList();
     }
 
 }
