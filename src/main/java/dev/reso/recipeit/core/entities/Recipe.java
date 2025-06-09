f93017031155756a04a2e96a2427b904b43f8d5c
Diego Reis <diegoreso@me.com>
2025-06-09 18:50:11 -0300

    refact: adds no args and all args conbtrucstor, adds getter setter

--- src/main/java/dev/reso/recipeit/core/entities/Recipe.java
+++ src/main/java/dev/reso/recipeit/core/entities/Recipe.java
@@ -15,4 +15,93 @@ public class Recipe {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private TypeRecipe type;
+
+    public Recipe() {
+    }
+
+    public Recipe(Long id, String name, String description, String instruction, double preparationTimeMinutes, double servings, LocalDateTime createdAt, LocalDateTime updatedAt, TypeRecipe type) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.instruction = instruction;
+        this.preparationTimeMinutes = preparationTimeMinutes;
+        this.servings = servings;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public double getPreparationTimeMinutes() {
+        return preparationTimeMinutes;
+    }
+
+    public void setPreparationTimeMinutes(double preparationTimeMinutes) {
+        this.preparationTimeMinutes = preparationTimeMinutes;
+    }
+
+    public double getServings() {
+        return servings;
+    }
+
+    public void setServings(double servings) {
+        this.servings = servings;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public TypeRecipe getType() {
+        return type;
+    }
+
+    public void setType(TypeRecipe type) {
+        this.type = type;
+    }
+
+
 }
