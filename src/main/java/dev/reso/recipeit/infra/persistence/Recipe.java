8d5e77daf5c2a608baa94710c6d31fe9bbec8e8a
Diego Reis <diegoreso@me.com>
2025-06-09 15:50:09 -0300

    feat: creates recipe persistence entity

---
+++ src/main/java/dev/reso/recipeit/infra/persistence/Recipe.java
@@ -0,0 +1,39 @@
+package dev.reso.recipeit.infra.persistence;
+
+import dev.reso.recipeit.core.enums.TypeRecipe;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "recipe")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    private String instruction;
+    private double preparationTimeMinutes;
+    private double servings;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private TypeRecipe type;
+}
