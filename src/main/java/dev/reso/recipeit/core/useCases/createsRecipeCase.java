8218621791d716a8d149ad374dd833b234c45813
Diego Reis <diegoreso@me.com>
2025-06-09 15:50:52 -0300

    feat: creates createsRecipeCase and createsRecipeImpl

---
+++ src/main/java/dev/reso/recipeit/core/useCases/createsRecipeCase.java
@@ -0,0 +1,9 @@
+package dev.reso.recipeit.core.useCases;
+
+import dev.reso.recipeit.core.entities.Recipe;
+
+public interface createsRecipeCase {
+
+    public Recipe execute(Recipe recipe);
+
+}
---
+++ src/main/java/dev/reso/recipeit/core/useCases/createsRecipeImpl.java
@@ -0,0 +1,11 @@
+package dev.reso.recipeit.core.useCases;
+
+import dev.reso.recipeit.core.entities.Recipe;
+
+public class createsRecipeImpl implements createsRecipeCase{
+
+    @Override
+    public Recipe execute(Recipe recipe) {
+        return null;
+    }
+}
