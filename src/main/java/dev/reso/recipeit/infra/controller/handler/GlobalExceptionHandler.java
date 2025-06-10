79367eaf8d40957172e82038a2085d43742d8a39
Diego Reis <diegoreso@me.com>
2025-06-10 20:33:07 -0300

    feat: adds ResourceNotFoundException on exception handler

--- src/main/java/dev/reso/recipeit/infra/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/dev/reso/recipeit/infra/controller/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package dev.reso.recipeit.infra.controller.handler;
 
 import dev.reso.recipeit.core.exceptions.DuplicateRecipeKeyException;
+import dev.reso.recipeit.core.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,5 +22,16 @@ public class GlobalExceptionHandler {
         err.setMessage(ex.getMessage());
         err.setPath(request.getRequestURI());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(err);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("resource not found");
+        err.setMessage(ex.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 }
