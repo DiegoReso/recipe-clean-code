c5063723bcdb16d41c8956164a3431c98c359a17
Diego Reis <diegoreso@me.com>
2025-06-11 21:01:01 -0300

    feat: adds FindRecipeByIdentification bean to context

--- src/main/java/dev/reso/recipeit/infra/beans/BeanConfiguration.java
+++ src/main/java/dev/reso/recipeit/infra/beans/BeanConfiguration.java
@@ -1,10 +1,7 @@
 package dev.reso.recipeit.infra.beans;
 
 import dev.reso.recipeit.core.gateway.RecipeGateway;
-import dev.reso.recipeit.core.useCases.CreatesRecipeUseCase;
-import dev.reso.recipeit.core.useCases.CreatesRecipeUseCaseImpl;
-import dev.reso.recipeit.core.useCases.FindRecipesUseCase;
-import dev.reso.recipeit.core.useCases.FindRecipesUseCaseImpl;
+import dev.reso.recipeit.core.useCases.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +16,10 @@ public class BeanConfiguration {
     @Bean
     public FindRecipesUseCase findRecipesUseCase(RecipeGateway recipeGateway){
         return new FindRecipesUseCaseImpl(recipeGateway);
+    }
+
+    @Bean
+    public FindRecipeByIdentification findRecipeByIdentification(RecipeGateway recipeGateway) {
+        return new FindRecipeByIdentificationImpl(recipeGateway);
     }
 }
