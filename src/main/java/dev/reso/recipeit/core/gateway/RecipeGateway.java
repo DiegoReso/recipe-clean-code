505c3cf13385b6793d34f246d1f8cff486ee660e
Diego Reis <diegoreso@me.com>
2025-06-16 16:04:26 -0300

    refact: Update RecipeGateway interface for exception handling. Declares `DuplicateRecipeKeyException` in `createsRecipe` method signature to reflect new exception propagation from gateway.

--- src/main/java/dev/reso/recipeit/core/gateway/RecipeGateway.java
+++ src/main/java/dev/reso/recipeit/core/gateway/RecipeGateway.java
@@ -1,12 +1,13 @@
 package dev.reso.recipeit.core.gateway;
 
 import dev.reso.recipeit.core.entities.Recipe;
+import dev.reso.recipeit.core.exceptions.DuplicateRecipeKeyException;
 
 import java.util.List;
 
 public interface RecipeGateway {
 
-    Recipe createsRecipe(Recipe recipe);
+    Recipe createsRecipe(Recipe recipe) throws DuplicateRecipeKeyException;
 
     List<Recipe> findRecipes();
 
