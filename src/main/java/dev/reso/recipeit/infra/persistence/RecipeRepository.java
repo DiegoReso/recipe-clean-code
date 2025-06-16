43576d684146806c478dee1a2b93bafb83983df3
Diego Reis <diegoreso@me.com>
2025-06-16 18:12:13 -0300

    refact: Update RecipeRepository to use Optional for find methods

--- src/main/java/dev/reso/recipeit/infra/persistence/RecipeRepository.java
+++ src/main/java/dev/reso/recipeit/infra/persistence/RecipeRepository.java
@@ -2,9 +2,11 @@ package dev.reso.recipeit.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
 
-    RecipeEntity findByIdentification(String identification);
+    Optional<RecipeEntity> findByIdentification(String identification);
 
 }
